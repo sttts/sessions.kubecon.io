498c36d1b7267250da2e9769f2f38ed0032cb01d
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-04-30 00:22:58 +0200

    Mark Session as runtime.Object for deepcopy

--- pkg/apis/kubecon.io/v1/types.go
+++ pkg/apis/kubecon.io/v1/types.go
@@ -4,6 +4,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+//
+// Session is a KubeCon session.
 type Session struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
